7800979419098c8591d802da40f6e428f02d9459
Nastazys <nastazys@gmail.com>
2019-03-03 23:38:34 +0300

    lab1 +

---
+++ lab1/src/sample/Main.java
@@ -0,0 +1,108 @@
+package sample;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public static final double PI = 3.1415;
+
+    @Override  public void start(Stage primaryStage) {
+        primaryStage.setResizable(true);
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 330, 145);
+        GridPane gridpane = new GridPane();
+        gridpane.setPadding(new Insets(5));
+        gridpane.setHgap(5);
+        gridpane.setVgap(5);
+        for (int i = 0; i < 2; i++)
+        {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setPercentWidth(100 / 2.0);
+            gridpane.getColumnConstraints().add(column);
+        }
+
+        Label label = new Label("Градусы");
+        gridpane.add(label, 0, 0);
+        TextField degreesTextField = new TextField();
+        gridpane.add(degreesTextField, 1, 0);
+
+        label = new Label("Радианы");
+        gridpane.add(label, 0, 1);
+        TextField radianTextField = new TextField();
+        gridpane.add(radianTextField, 1, 1);
+        Button calculateRadButton = new Button("Посчитать радианы");
+        gridpane.add(calculateRadButton, 0, 3);
+
+        Button calculateDegButton = new Button("Посчитать градусы");
+        gridpane.add(calculateDegButton, 1, 3);
+
+
+        Label angleTypeLabel = new Label (" ");
+        gridpane.add(angleTypeLabel, 0, 2);
+
+        calculateRadButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            public void handle(ActionEvent e)
+            {
+                Double Degrees = Double.parseDouble(degreesTextField.getText());
+                Double Radians = new BigDecimal(Degrees*PI / 180).setScale(4, RoundingMode.HALF_UP).doubleValue();
+                if(Degrees >= 0 && Degrees < 90)
+                    angleTypeLabel.setText("Острый угол");
+                else if(Degrees == 90)
+                    angleTypeLabel.setText("Прямой угол");
+                else if(Degrees < 180)
+                    angleTypeLabel.setText("Тупой угол");
+                else if(Degrees == 180)
+                    angleTypeLabel.setText("Развёрнутый угол");
+                else {
+                    angleTypeLabel.setText("Ошибка! Введите угол до 180 градусов!");
+                    return;
+                }
+                radianTextField.setText(String.valueOf(Radians));
+            }
+        });
+
+        calculateDegButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            public void handle(ActionEvent e)
+            {
+                Double Radians = Double.parseDouble(radianTextField.getText());
+                Integer Degrees = new BigDecimal(Radians * 180 / PI).setScale(0, RoundingMode.HALF_UP).intValue();
+                if(Degrees >= 0 && Degrees < 90)
+                    angleTypeLabel.setText("Острый угол");
+                else if(Degrees == 90)
+                    angleTypeLabel.setText("Прямой угол");
+                else if(Degrees < 180)
+                    angleTypeLabel.setText("Тупой угол");
+                else if(Degrees == 180)
+                    angleTypeLabel.setText("Развёрнутый угол");
+                else {
+                    angleTypeLabel.setText("Ошибка! Введите угол до 180 градусов!");
+                    return;
+                }
+                degreesTextField.setText(String.valueOf(Degrees));
+            }
+        });
+
+        root.setCenter(gridpane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
